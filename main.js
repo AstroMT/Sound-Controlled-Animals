850e8e9455122976d09e85ada169425252c8248a
AstroMT <81778387+AstroMT@users.noreply.github.com>
2022-02-06 10:14:53 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-//
+function start() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/OCAbwVKfN/model.json', modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    console.log("got results")
+}
